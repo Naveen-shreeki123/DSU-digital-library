412827a96caaf84378960f4761dea67201622cdc
Naveen A <67964411+Naveen-shreeki123@users.noreply.github.com>
2021-07-28 15:02:05 +0530

    Add files via upload

---
+++ src/routes/classroom/Classroom.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import './class.css';
+import {Row,Col,Container} from 'react-bootstrap';
+
+function Classroom() {
+    return (
+        <div className='classroom'>
+            <div className='box1'>
+                <h1>CLASSROOM</h1>
+        </div>
+        <Container>
+            <Row>
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            </Row>
+
+            <Row>
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            </Row>
+
+            <Row>
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM" frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            <Col lg={4} md={12} sm={12} xs={12} className='set1'>
+
+                <div className="embed-responsive embed-responsive-16by9">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"frameBorder='10' allowFullScreen></iframe>
+                </div>
+                <p className='description'>Applied Science</p>
+            </Col>
+
+            </Row>
+        </Container>
+        <footer className="PageFooter">
+                <div className="footer-copyright text-center py-3">Created and designed by:
+                 fantastic four
+                </div>
+        </footer>
+        </div>
+    );
+}
+export default Classroom;
